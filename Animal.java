2517b2e32968b8475286726fe962383610d5d287
Nicholas Fafel <nicholasfafel@wifi-roaming-128-4-95-92.host.udel.edu>
2019-09-16 17:46:08 -0400

    made commented explainations for the Animal class

--- Animal.java
+++ Animal.java
@@ -1,13 +1,17 @@
 class Animal {
+    //Every Animal object, no matter the kind of animal, will have a name and a number of legs
     private int numLegs;
     String name;
 
     public Animal(String name, int numLegs) {
+        /* This constructor takes two variable which are the name of the animal, a String, and the number of legs of the animal, an int.
+         * This constructor then sets the name and numLegs attributes with the appropriate argument values. */
         this.numLegs = numLegs;
         this.name = name;
     }
     
     public int getLegs() {
+        //This is a getter method for the private attribute numLegs.
         return numLegs;
     }
 }
