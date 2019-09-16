4027a444d7e0ea84acd9126a5d58f3bf7f15bddb
Nicholas Fafel <nicholasfafel@wifi-roaming-128-4-95-92.host.udel.edu>
2019-09-16 17:47:30 -0400

    made commented explainations for the Dog class

--- Dog.java
+++ Dog.java
@@ -1,10 +1,13 @@
 class Dog extends Animal {
 
     public Dog(String name, int numLegs) {
+        //The Dog class constructor just calls the constructor of its super class Animal using the same arguments as passed into the Dog class constructor.
         super(name, numLegs);
     }
 
+    @Override
     public String toString() {
+        //This toString method overrides the Object toString method and returns the name of the dog object. 
         return name;
     }
 
